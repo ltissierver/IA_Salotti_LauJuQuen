9734c793e39e14c531d48cf7be82dd8d4226188f
Juliette Sta <jsta@ensc.fr>
2018-03-09 14:20:41 +0100

    node

--- Projet_IA_Quentin_Juliette_Laurie/Projet_IA_Quentin_Juliette_Laurie/Node.cs
+++ Projet_IA_Quentin_Juliette_Laurie/Projet_IA_Quentin_Juliette_Laurie/Node.cs
@@ -8,6 +8,14 @@ namespace Projet_IA_Quentin_Juliette_Laurie
 {
     class Node : GenericNode
     {
+        public int x; // Lignes
+        public int y; // Colonnes
+
+        public Node (int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
         
 
         public override double CalculeHCost()
@@ -27,7 +35,16 @@ namespace Projet_IA_Quentin_Juliette_Laurie
 
         public override List<GenericNode> GetListSucc()
         {
-             
+            List<GenericNode> lsucc = new List<GenericNode>();
+            // Regarder à droite
+            if ( Form1.matrice[x,y+1] == 1)
+            {
+                lsucc.Add(new Node(x, y + 1));
+            }
+            // Regarder à Gauche
+            // Regarder en haut
+            // Regarder à gauche
+            // Regarder en diagonale
             throw new NotImplementedException();
         }
 
